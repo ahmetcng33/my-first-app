01015aba7d0199e1a0d959be667513a3a0b1f787
ahmetcng33 <ahmetcingi333@gmail.com>
2022-10-11 15:46:59 +0300

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const a = 5;
+const element = (
+<>
+    <h1>Hello React {a}</h1>
+    <p>Merhaba</p>
+</>
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(element);
 
 
